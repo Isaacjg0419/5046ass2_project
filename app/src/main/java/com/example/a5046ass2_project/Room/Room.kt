306b645fc35a7e23eb2558f18966107c7ddabf47
Isaac0419 <113324504+Isaac0419@users.noreply.github.com>
2023-05-07 22:28:35 +1000

    demo stack again

---
+++ app/src/main/java/com/example/a5046ass2_project/Room/Room.kt
@@ -0,0 +1,86 @@
+package com.example.a5046ass2_project.Room
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.a5046ass2_project.Room.entity.Customer
+import com.example.a5046ass2_project.Room.viewmodel.CustomerViewModel
+import com.example.a5046ass2_project.databinding.ActivityRoomBinding
+
+class Room : AppCompatActivity() {
+    private lateinit var binding: ActivityRoomBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRoomBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+        binding.idTextField.setPlaceholderText("This is only used for Edit")
+        val customerViewModel: CustomerViewModel =
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(
+                CustomerViewModel::class.java
+            )
+        customerViewModel.allCustomers.observe(this, Observer { customers ->
+            var allCustomers = ""
+            for (temp in customers) {
+                val customerDetails: String =
+                    temp.uid.toString() + " " + temp.firstName + " " +
+                            temp.lastName + " " + temp.salary
+                allCustomers =
+                    allCustomers + System.getProperty("line.separator") +
+                            customerDetails
+            }
+            binding.textViewRead.setText("All data: " + allCustomers)
+        })
+
+        binding.addButton.setOnClickListener {
+            val name = binding.nameTextField.editText!!.text.toString()
+            val surname = binding.surnameTextField.editText!!.text.toString()
+            val strSalary = binding.salaryTextField.editText!!.text.toString()
+            if (!name.isEmpty() && !surname.isEmpty() && !strSalary.isEmpty()) {
+                val salary = strSalary.toDouble()
+                val customer = Customer(name, surname, salary)
+                customerViewModel.insert(customer)
+                binding.textViewAdd.text = "Added Record: $name $surname $salary"
+            }
+        }
+        binding.deleteButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+
+                18 of 18
+                customerViewModel.deleteAll()
+                binding.textViewDelete.setText("All data was deleted")
+            }
+        })
+        binding.clearButton.setOnClickListener {
+            binding.nameTextField.editText!!.setText("")
+            binding.surnameTextField.editText!!.setText("")
+            binding.salaryTextField.editText!!.setText("")
+        }
+        binding.updateButton.setOnClickListener {
+            val strId = binding.idTextField.editText!!.text.toString()
+            var id = 0
+            if (!strId.isEmpty()) id = strId.toInt()
+            val name = binding.nameTextField.editText!!.text.toString()
+            val surname = binding.surnameTextField.editText!!.text.toString()
+            val strSalary = binding.salaryTextField.editText!!.text.toString()
+            if (!name.isEmpty() && !surname.isEmpty() && !strSalary.isEmpty()) {
+                val doubleSalary = strSalary.toDouble()
+                customerViewModel.findCustomerById(id).observe(this, Observer { result ->
+                    var customer = result
+                    if (customer != null) {
+                        customer.firstName = name
+                        customer.lastName = surname
+                        customer.salary = doubleSalary
+                        customerViewModel.update(customer)
+                        binding.textViewUpdate.text =
+                            "Update was successful for ID: " + customer.uid
+                    } else {
+                        binding.textViewUpdate.text = "Id does not exist"
+                    }
+                })
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a5046ass2_project/Room/repository/CustomerRepository.kt
@@ -0,0 +1,31 @@
+package Respository
+
+import android.app.Application
+import com.example.a5046ass2_project.Room.dao.CustomerDAO
+import com.example.a5046ass2_project.Room.database.CustomerDatabase
+import com.example.a5046ass2_project.Room.entity.Customer
+import kotlinx.coroutines.flow.Flow
+
+class CustomerRepository(application: Application) {
+    private var customerDao: CustomerDAO =
+        CustomerDatabase.CustomerDatabase.getInstance(application).customerDao()
+    //allcustomers public property so it can be called from viewmodel
+    val allcustomers: Flow<List<Customer>> = customerDao.getAll()
+
+    suspend fun findById(customerId: Int): Customer? {
+        return customerDao.findByID(customerId)
+    }
+
+    suspend fun insert(customer: Customer) {
+        customerDao.insert(customer)
+    }
+    suspend fun delete(customer: Customer) {
+        customerDao.delete(customer)
+    }
+    suspend fun deleteAll() {
+        customerDao.deleteAll()
+    }
+    suspend fun update(customer: Customer) {
+        customerDao.update(customer)
+    }
+}
