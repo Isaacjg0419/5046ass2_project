6c1a003347feffabc876f4e29a5e353442359f31
Isaac0419 <113324504+Isaac0419@users.noreply.github.com>
2023-05-08 20:45:59 +1000

    finally add the marker and centered the marker

--- app/src/main/java/com/example/a5046ass2_project/GoogleMap/AttributeDao.kt
+++ app/src/main/java/com/example/a5046ass2_project/GoogleMap/AttributeDao.kt
@@ -2,20 +2,20 @@ package com.example.a5046ass2_project.GoogleMap
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.OnConflictStrategy
+
+
 
 
 @Dao
 interface AttributeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(attribute: Attribute)
+
     @Query("SELECT * FROM attributes")
-    fun getAllAttributes():LiveData< List<Attribute>>
-
-    @Insert
-    fun insertAttribute(attribute: Attribute)
-
-    @Delete
-    fun deleteAttribute(attribute: Attribute)
+    fun getAllAttributes(): List<Attribute>
 }
+
 
--- app/src/main/java/com/example/a5046ass2_project/GoogleMap/GMapsActivity.kt
+++ app/src/main/java/com/example/a5046ass2_project/GoogleMap/GMapsActivity.kt
@@ -1,11 +1,10 @@
 package com.example.a5046ass2_project.GoogleMap
+
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
+
 import androidx.room.Room
 import com.example.a5046ass2_project.R
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,9 +14,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.CameraPosition
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
 
-class GMapsActivity: AppCompatActivity(), OnMapReadyCallback {
-
+class GMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var googleMap: GoogleMap
     private lateinit var attributeDao: AttributeDao
 
@@ -25,57 +28,58 @@ class GMapsActivity: AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gmaps)
 
-        // 初始化 Room 数据库
-        val db = Room.databaseBuilder(
+        attributeDao = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java,
             "app_database"
-        ).build()
-        attributeDao = db.attributeDao()
+        ).build().attributeDao()
 
-        // 获取地图对象
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
-        googleMap.uiSettings.setZoomControlsEnabled(true)
+        googleMap.uiSettings.isZoomControlsEnabled = true
 
-        // 请求位置权限
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            googleMap.isMyLocationEnabled = true
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-        }
+        val monashClaytonLatLng = LatLng(-37.915, 145.135)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(monashClaytonLatLng, 14f))
 
-        // 设置墨尔本为地图的中心点
-        val melbourne = LatLng(-37.8136, 144.9631)
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourne, DEFAULT_ZOOM))
-        val markerOptions = MarkerOptions().position(melbourne).title("Melbourne CBD")
+        loadJsonDataAndRenderMarkers()
+    }
 
-        googleMap.addMarker(markerOptions)
+    private fun loadJsonDataAndRenderMarkers() {
+        val inputStream = assets.open("property_data.json")
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
 
-        // 添加属性数据到地图
-        attributeDao.getAllAttributes().observe(this, Observer { attributes ->
-            attributes.forEach { attribute ->
-                val position = LatLng(attribute.latitude, attribute.longitude)
-                val markerOptions = MarkerOptions().position(position).title(attribute.title)
-                googleMap.addMarker(markerOptions)
+        val jsonArray = JSONArray(jsonString)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                val title = jsonObject.getString("title")
+                val description = jsonObject.getString("description")
+                val latitude = jsonObject.getDouble("latitude")
+                val longitude = jsonObject.getDouble("longitude")
+
+                val latLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions().position(latLng).title(title).snippet(description)
+
+                withContext(Dispatchers.Main) {
+                    googleMap.addMarker(markerOptions)
+                }
+
+                val attribute = Attribute(
+                    title = title,
+                    description = description,
+                    latitude = latitude,
+                    longitude = longitude
+                )
+                attributeDao.insert(attribute)
             }
-        })
+        }
     }
 
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 123
-        private const val DEFAULT_ZOOM = 10f
-    }
+
 }
