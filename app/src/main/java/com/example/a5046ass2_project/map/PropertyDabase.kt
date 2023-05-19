5f854955339e33dc38d4c81fe18e786abc6209b6
Isaac0419 <113324504+Isaac0419@users.noreply.github.com>
2023-05-19 21:00:50 +1000

    add type converters to room database

--- app/src/main/java/com/example/a5046ass2_project/map/PropertyDabase.kt
+++ app/src/main/java/com/example/a5046ass2_project/map/PropertyDabase.kt
@@ -2,14 +2,18 @@ package com.example.a5046ass2_project.map
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
+
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.a5046ass2_project.profile.Converters
 import com.example.a5046ass2_project.profile.ProfileDAO
+import com.example.a5046ass2_project.profile.UserProfile
 
-@Database(entities = [Property::class], version = 5, exportSchema = false)
+@Database(entities = [Property::class, UserProfile::class], version = 6, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class PropertyDatabase : RoomDatabase() {
-    abstract fun propertyDAO(): PropertyDAO
+    abstract fun propertyDAO():PropertyDAO
     abstract fun profileDAO(): ProfileDAO
-
     companion object {
         @Volatile
         private var instance: PropertyDatabase? = null
@@ -30,5 +34,3 @@ abstract class PropertyDatabase : RoomDatabase() {
         }
     }
 }
-
-
